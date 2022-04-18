a7f6962c8cf42431c2e0c253659e96fb9f9fa5f6
davidh <iq.davidh@gmail.com>
2022-04-18 05:32:42 -0500

    using context

--- src/components/CDateSelector.js
+++ src/components/CDateSelector.js
@@ -3,42 +3,44 @@ import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min';
 import "bulma-calendar/dist/css/bulma-calendar.min.css"
 import './cdateselector.css';
 import DateContext from "../model/DateContext";
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CDateSelector = ({}) => {
   
   const modelDate = useContext(DateContext)
   
+  const setDatePicket = (datepicker) => {
+    const v = datepicker.data.value();
+    const isValid = modelDate.getIsApocDateValid(v);
+    if (isValid === true) {
+      modelDate.setDate(v)
+    } else {
+      toast(isValid);
+    }
+  }
+
   
   useEffect(() => {
     // Initialize all input of date type.
-    bulmaCalendar.attach('[type="date"]', {dateFormat:"yyyy-MM-dd"});
+    bulmaCalendar.attach('[type="date"]', {dateFormat: "yyyy-MM-dd"});
     
     // To access to bulmaCalendar instance of an element
     // eslint-disable-next-line no-undef
     const element = document.querySelector('#datetxt');
     // bulmaCalendar instance is available as element.bulmaCalendar
-    element.bulmaCalendar.on('select', (datepicker) => {
-      const v=datepicker.data.value();
-      const isValid = modelDate.getIsApocDateValid(v );
-      if(isValid===true){
-        modelDate.setDate(v)
-      }else{
-        console.log('wrong date');
-      }
-      
-    });
-    element.bulmaCalendar.color="#012b6c"
-    element.bulmaCalendar.dateFormat="yyyy-MM-dd"
+    element.bulmaCalendar.on('select', setDatePicket);
+    
     
   }, []);
   
   return (
-    <div className="is-flex mb-4" style={{justifyContent:"center"}}>
-      <div style={{maxWidth:"180px"}}>
-        <input id="datetxt" type="date" />
+    <div className="is-flex mb-4" style={{justifyContent: "center"}}>
+      <div style={{maxWidth: "180px"}}>
+        <input id="datetxt" type="date"/>
         {modelDate.getDate()}
       </div>
-      
+      <ToastContainer/>
     </div>
   );
 };
