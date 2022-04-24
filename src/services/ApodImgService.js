08ef4f0e558fb004ab7e9cb2781e76cab8acfdcb
davidh <iq.davidh@gmail.com>
2022-04-24 06:20:59 -0500

    calenda working

--- src/services/ApodImgService.js
+++ src/services/ApodImgService.js
@@ -3,22 +3,29 @@ import apiClient from "./NasaApiClient";
 const KEY_LOCALSTORAGE_APOD = "apodcache";
 
 
-const getListInLocaslStorage = () => {
+const getCache = () => {
   const json = window.localStorage.getItem(KEY_LOCALSTORAGE_APOD) || "[]";
   return JSON.parse(json);
 }
 
-const getItemFromLocalStorage = (date) => {
+const getItemFromCache = (date) => {
   if (!date) {
     return null;
   }
-  const list = getListInLocaslStorage();
+  const list = getCache();
   return list.find(k => k.date === date);
 };
 
 
 const addItemToLocalStorage = (item) => {
-  let list = getListInLocaslStorage();
+  
+  const itemInCache = getItemFromCache(item.date);
+  
+  if (itemInCache) {
+    return;
+  }
+  
+  let list = getCache();
   list = [...list, item];
   window.localStorage.setItem(KEY_LOCALSTORAGE_APOD, JSON.stringify(list));
 }
@@ -28,18 +35,21 @@ const ApodImgService = {
   
   getItemFromDate: async (date) => {
     
-    const itemCache = getItemFromLocalStorage(date);
+    const itemCache = getItemFromCache(date);
     
-    if (!itemCache) {
-      const result = await apiClient.Apod(date);
-      
-      if (!result.success) {
-        throw  new Error(result.msg);
-      }
-      const item = result.data;
-      addItemToLocalStorage(item);
-      return item;
+    
+    if (itemCache) {
+      return itemCache;
     }
+    
+    const result = await apiClient.Apod(date);
+    
+    if (!result.success) {
+      throw  new Error(result.msg);
+    }
+    const item = result.data;
+    addItemToLocalStorage(item);
+    return item;
     
   }
 }
