785ac40587b9ed3db6ef4450a2b2dfd3ce1db052
davidh <iq.davidh@gmail.com>
2022-04-24 05:22:01 -0500

    calenda working

--- src/components/CModalSelDate.js
+++ src/components/CModalSelDate.js
@@ -1,73 +1,38 @@
 import React, {useContext, useEffect} from 'react';
 import AppContext from "../model/AppContext";
-import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min';
-import "bulma-calendar/dist/css/bulma-calendar.min.css"
-import './cdateselector.css';
 import 'react-toastify/dist/ReactToastify.css';
-import {toast, ToastContainer} from "react-toastify";
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
 
 
 const CModalSelDate = ({}) => {
   
   const modelAppState = useContext(AppContext);
+
   
-  useEffect(() => {
-    // Initialize all input of date type.
-    const calOptions={dateFormat: "yyyy-MM-dd", showHeader:false, displayMode:"dialog"}
-    bulmaCalendar.attach('[type="date"]', calOptions);
-    
-    // To access to bulmaCalendar instance of an element
-    // eslint-disable-next-line no-undef
-    const element = document.querySelector('#datetxt');
-    // bulmaCalendar instance is available as element.bulmaCalendar
-    element.bulmaCalendar.on('select', setDatePicket);
-    element.bulmaCalendar.show();
-    
-  }, []);
-  
-  const setDatePicket = (datepicker) => {
-    const v = datepicker.data.value();
-    const isValid = modelAppState.getIsApocDateValid(v);
-    if (isValid === true) {
-      modelAppState.setDate(v)
-    } else {
-      toast(isValid);
-    }
-  }
-  
-  
-  const fnClose=()=>{
+  const fnClose = () => {
     modelAppState.setIsShowModal(false);
   }
+  
+  const onChange = (d) => {
+    const day = d.getDate().toString().padStart(2,'0');
+    const month = (d.getMonth() +1).toString().padStart(2,'0');
+    const y = d.getFullYear();
+    
+    const ymd=`${y}-${month}-${day}`;
+    
+ 
+    modelAppState.setIsShowModal(false);
+    
+  }
+  
   return (
     <>
-      <div className="modal is-active" >
-        <div className="modal-background" onClick={fnClose}/>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">
-              <i className="fa fa-calendar mr-2"/>
-              Select picture from date
-            </p>
-            <button
-              onClick={fnClose}
-              className="delete"
-              aria-label="close"
-            />
-          </header>
-          <section className="modal-card-body">
-            <div className="calWrapper">
-              <input id="datetxt" type="date"/>
-            </div>
-            <ToastContainer/>
-          </section>
-          <footer className="modal-card-foot has-text-right">
-            <button onClick={fnClose} className="button is-info">
-              <i className="fa fa-times mr-2"/>Close
-            </button>
-          </footer>
-        </div>
-        
+      <div className="modal is-active">
+        <div className="modal-background"
+             style={{zIndex: -9999}}
+             onClick={fnClose}/>
+        <Calendar onChange={onChange}/>
       </div>
     </>
   );
